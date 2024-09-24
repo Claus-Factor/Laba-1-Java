ac97379442ea6686a5f6a5b3c68086593ace129d
Nicholas527 <volxin.k2004@gmail.com>
2024-09-24 20:59:04 +0300

    Add GET-method for the last element

--- Container.java
+++ Container.java
@@ -18,22 +18,28 @@ public class Container<T> {
         this.size = 0;
         this.arr = (T[]) new Object[capacity];
     }
+    // Конструкток по умолчанию устанавливает емкость в 10 элементов
     public Container() {
        this(10);
     }
 
+    // Добавляем элемент в конец контейнера
     public void add(T element) {
         if (size == capacity) {
             this.resize();
         }
         arr[size] = element;
     }
+
+    // Считываем значение n-го элемента
     public T get(int number) {
         if (number <= size && number >= 0) {
             return arr[number];
         } else return null;
     }
-
+    public T get() {
+        return get(size-1);
+    }
 
 
 }
