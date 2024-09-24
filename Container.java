51d7f7f25883be5918eeb6d5d2ab32de4d253ea9
Nicholas527 <volxin.k2004@gmail.com>
2024-09-24 20:46:37 +0300

    Add RESIZE-method

--- Container.java
+++ Container.java
@@ -3,6 +3,16 @@ public class Container<T> {
     private int size; // Текущий размер контейнера
     private T[] arr;
 
+    // Увеличение максимальной ёмкости хранилища в случае переполнения
+    private void resize() {
+        capacity *= 2;
+        T[] tempArr = (T[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            tempArr[i] = arr[i];
+        }
+        arr = tempArr;
+    }
+
     public Container(int capacity) {
         this.capacity = capacity;
         this.size = 0;
