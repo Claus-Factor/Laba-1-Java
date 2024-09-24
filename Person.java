2a957ea2d38983223dcadef4ba9237081a906633
Nicholas527 <volxin.k2004@gmail.com>
2024-09-24 20:30:26 +0300

    Create Main.class, create Examlpe class Person for demostration

---
+++ Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    public String name;
+    public int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void display() {
+        System.out.println("Имя: " + name + ", Возраст: " + age);
+    }
+
+}
